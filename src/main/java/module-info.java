fbde6719b372f3fa0ca54de05046cfe298228a17
Karl Vince Reyes <98654168+karlwizkrafte@users.noreply.github.com>
2025-07-26 17:15:29 +0800

    feat: added sqlite

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module cpe121.group3 {
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
     requires transitive javafx.graphics;
+    requires java.sql;
 
     opens cpe121.group3 to javafx.fxml;
     exports cpe121.group3;
