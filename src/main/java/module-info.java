eaf1180fd0347bd32e461bfe206765084568760f
Karl Vince Reyes <98654168+karlwizkrafte@users.noreply.github.com>
2025-08-21 21:25:03 +0800

    fix: add missing java.prefs requirement in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module cpe121.group3 {
     requires transitive javafx.fxml;
     requires transitive javafx.graphics;
     requires java.sql;
+    requires java.prefs;
 
     opens cpe121.group3 to javafx.fxml;
     exports cpe121.group3;
